7c5b8119aeb65df26b53beecbfddeb7d3646e006
Dmitry <56439852+Amilotxak@users.noreply.github.com>
2025-10-29 01:18:14 +0300

    Добавляет разметку сайта и начало стилизации блоков

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,10 @@
 /* в этот файл добавляет скрипты*/
+let navMain = document.querySelector('.header__button-wrapper');
+let navToggle = document.querySelector('.header__toggle');
+
+navMain.classList.remove('header__button-nojs');
+
+navToggle.addEventListener('click', function () {
+  navMain.classList.toggle('header__button-closed');
+  navMain.classList.toggle('header__button-opened');
+});
